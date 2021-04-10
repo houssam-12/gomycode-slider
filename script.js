4696bcf0413f73213f45c8b4c2a9f77d171a638f
Houssam <metatlahoussam@gmail.com>
2021-04-10 12:07:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var images = [
+    'im01.jpg',
+    'im02.jpg',
+    'im03.jpg',
+    'im04.jpg',
+    'im05.jpg',
+]
+var cpt = 0
+
+function change() {
+    var img = document
+        .getElementById('img-slider')
+    img.setAttribute('src', 'img/' + images[cpt])
+}
+
+setInterval(() => {
+    change();
+    cpt++;
+    if (cpt > 4) cpt = 0;
+}, 4000)
+
+
+function next() {
+    cpt++;
+    if (cpt > 4) cpt = 0;
+    change()
+}
+
+function previous() {
+    cpt--;
+    if (cpt < 0) cpt = 0;
+    change()
+}
